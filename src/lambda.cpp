8d2b4f19a90539a9c0fac1c95401a8c837ad146e
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2012-12-28 16:07:48 +0100

    pinknoise

--- src/lambda.cpp
+++ src/lambda.cpp
@@ -19,6 +19,9 @@
 //  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //  or visit http://www.gnu.org/licenses/gpl.html
 
+
+// SAMPLES 
+
 #include "lambda.h"
 
 using namespace std;
