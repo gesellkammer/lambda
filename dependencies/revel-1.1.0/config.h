a7db0e7db862370f13683c3995d2e0cbe49d3184
Eduardo Moguillansky <em@eusebius.(none)>
2014-02-22 01:12:08 +0100

    added librevel to the tree

---
+++ dependencies/revel-1.1.0/config.h
@@ -0,0 +1,32 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* define if the compiler implements namespaces */
+#define HAVE_NAMESPACES /**/
+
+/* define if the compiler supports ISO C++ standard library */
+#define HAVE_STD /**/
+
+/* define if the compiler supports Standard Template Library */
+#define HAVE_STL /**/
+
+/* Name of package */
+#define PACKAGE "revel"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME ""
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING ""
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION ""
+
+/* Version number of package */
+#define VERSION "1.1.0"
--- src/moc_lambda.cpp
+++ src/moc_lambda.cpp
@@ -1,8 +1,8 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'lambda.h'
 **
-** Created: Sat Dec 29 16:09:34 2012
-**      by: The Qt Meta Object Compiler version 63 (Qt 4.8.2)
+** Created: Fri Feb 21 22:03:46 2014
+**      by: The Qt Meta Object Compiler version 63 (Qt 4.8.3)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -11,7 +11,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'lambda.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 63
-#error "This file was generated using the moc from 4.8.2. It"
+#error "This file was generated using the moc from 4.8.3. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
